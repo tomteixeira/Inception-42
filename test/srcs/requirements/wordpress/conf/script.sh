4acb323539a5f30cdfc644009b2485c861b8c80c
Tom Teixeira <tomtomteixeira3006@gmail.com>
2024-05-10 14:32:35 +0200

    new mariadb config

--- test/srcs/requirements/wordpress/conf/script.sh
+++ test/srcs/requirements/wordpress/conf/script.sh
@@ -77,6 +77,8 @@ wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html/
 wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html/
 wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html/
 wp config set DB_HOST mariadb --allow-root --path=/var/www/html/
+wp core install --url=$DOMAIN_NAME --title=inception --admin_user=$ADMIN_USER --admin_password=$ADMIN_PSWD --admin_email=$ADMIN_EMAIL --allow-root
+ wp user create $WP_USER $WP_EMAIL_USER --role=author --user_pass=$WP_PSWD_USER --allow-root --path=/var/www/html
 
 while ! wp db check --allow-root --path=/var/www/html/; do
     echo "Waiting for Database to be ready..."
@@ -85,4 +87,4 @@ while ! wp db check --allow-root --path=/var/www/html/; do
 done
 
 # Start php-fpm
-php-fpm -F
+php-fpm7.4 -F
